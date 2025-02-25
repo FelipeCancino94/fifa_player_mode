5aba8ec4291c5ee1ba2c564ae9c1d5d353e09923
Felipe <jfelipecr94@gmail.com>
2025-02-25 15:11:44 -0500

    add final styles and data for component seasons

--- src/app/components/MainMenu/MainMenu.tsx
+++ src/app/components/MainMenu/MainMenu.tsx
@@ -13,12 +13,12 @@ export function MainMenu() {
 
     return (
         <>
-            <div className={`button-menu w-[20px] absolute right-[16px] top-[16px] z-10 ${opened ? 'close-icon' : 'open-icon'}`} onClick={toggleMenu}>
+            <div className={`button-menu w-[20px] absolute right-[16px] top-[16px] z-20 ${opened ? 'close-icon' : 'open-icon'}`} onClick={toggleMenu}>
                 <div className={`line-hamb-menu bg-neutral-50 transition-all border-2 w-full mt-1 ${ opened ? 'border-neutral-50' : 'border-neutral-900' }`}></div>
                 <div className={`line-hamb-menu bg-neutral-50 transition-all border-2 w-full mt-1 ${ opened ? 'border-neutral-50' : 'border-neutral-900' }`}></div>
                 <div className={`line-hamb-menu bg-neutral-50 transition-all border-2 w-full mt-1 ${ opened ? 'border-neutral-50' : 'border-neutral-900' }`}></div>
             </div>
-            <div className={ `main-menu fixed top-0 w-full h-screen bg-neutral-900/60 p-16 flex justify-center transition-all ${opened ? 'left-0' : '-left-full'}` }>
+            <div className={ `main-menu fixed top-0 w-full h-screen bg-neutral-900/90 p-16 flex justify-center transition-all z-10 ${opened ? 'left-0' : '-left-full'}` }>
                 <ul>
                     <li>
                         <Link href="/" className="text-neutral-50 text-5xl text-center block underline my-8" onClick={toggleMenu}>Home</Link>

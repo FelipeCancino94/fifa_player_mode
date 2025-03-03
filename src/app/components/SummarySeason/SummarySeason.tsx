6120f417d695ff28429b0fc47c15dc3682a430e0
Felipe <jfelipecr94@gmail.com>
2025-03-03 12:48:19 -0500

    add success message when form is sended

--- src/app/components/SummarySeason/SummarySeason.tsx
+++ src/app/components/SummarySeason/SummarySeason.tsx
@@ -1,3 +1,4 @@
+export const fetchCache = 'force-no-store';
 interface SummarySeasonProps {
   team: string,
   assists: number,
--- src/app/end-season/page.tsx
+++ src/app/end-season/page.tsx
@@ -1,25 +1,20 @@
-import { neon } from "@neondatabase/serverless";
+"use client"
+import { useState } from "react";
+import SaveSeason from "../components/SaveSeason/SaveSeason";
+import Link from "next/link";
 
-export default async function Page() {
-    const baseUrl:string = process.env.DATABASE_URL || '';
+export default function Page() {
+    const [stateForm, setStateForm] = useState<boolean>(false);
 
-    async function create(formData: FormData) {
-        "use server";
-        const sql = neon(baseUrl);
-        const season = formData.get("season");
-        const team = formData.get("team");
-        const goals = parseInt(formData.get("goals") as string);
-        const assists = parseInt(formData.get("assists") as string);
-        const games = parseInt(formData.get("games") as string);
-        const red_cards = parseInt(formData.get("red_cards") as string);
-        const yellow_cards = parseInt(formData.get("yellow_cards") as string);
-        const team_week = parseInt(formData.get("team_week") as string);
-        const player_month = parseInt(formData.get("player_month") as string);
-        const player_year = formData.has("player_year");
-        const trophies = formData.get("trophies");
-        const team_colors = formData.get("team_colors");
-
-        await sql(`INSERT INTO stats (season, team, goals, assists, games, red_cards, yellow_cards, team_week, player_month, player_year, trophies, team_colors) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`, [season, team, goals, assists, games, red_cards, yellow_cards, team_week, player_month, player_year, trophies, team_colors]);
+    const sendForm = (formData: FormData) => {
+        SaveSeason(formData)
+            .then((response) => {
+                if (response) {
+                    setStateForm(true);
+                }
+            }).catch((error) => {
+                console.error(error);
+            });
     }
 
     const styles:string = "py-2 px-4 mb-4 rounded-md bg-transparent border-2 border-solid border-white placeholder:text-white w-full outline-0 text-white focus:bg-white focus:text-sky-700 focus:placeholder:text-sky-700 transition-all";
@@ -27,7 +22,7 @@ export default async function Page() {
     return (
         <div className="end-season p-4 bg-sky-700 min-h-screen">
             <h3 className="text-3xl text-center py-8 text-white">Finalizar temporada</h3>
-            <form action={ create }>
+            <form action={ sendForm } className={`form ${!stateForm ? 'block' : 'hidden'}`}>
                 <div className="flex justify-between gap-4">
                     <input className={ styles } type="text" placeholder="Temporada" name="season" />
                 </div>
@@ -56,11 +51,18 @@ export default async function Page() {
                     <input className={ styles } type="text" placeholder="Trofeos" name="trophies" />
                 </div>
                 <div className="flex justify-between gap-4">
-                    <button className={ `${styles} bg-white text-sky-700 uppercase font-bold hover:bg-transparent hover:text-white` } type="submit" name="send">
+                    <button className={ `${styles} bg-white !text-sky-700 uppercase font-bold hover:bg-transparent hover:!text-white` } type="submit" name="send">
                         Finalizar temporada
                     </button>
                 </div>
             </form>
+            <div className={`message ${stateForm ? 'block' : 'hidden'}`}>
+                <div className="content-message bg-white p-8 rounded-md">
+                    <h2 className="text-3xl text-center text-sky-700">Guardado !</h2>
+                    <p className="text-lg text-center text-sky-700 leading-none mt-4 mb-8">La temporada ha sido registrada éxitosamente.</p>
+                    <Link href="/" className="p-4 bg-sky-700 rounded-md text-white mx-auto block text-center border-2 border-solid border-sky-700 hover:text-sky-700 hover:bg-white transition-all">Ir a la página de inicio</Link>
+                </div>
+            </div>
         </div>
     )
 }

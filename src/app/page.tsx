56a6e068ea8be37103d652244f9bda4fe1aad556
Felipe <jfelipecr94@gmail.com>
2025-03-10 14:28:07 -0400

    add page file from manager mode and fix styles in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className='h-screen'>
       <ul className="flex justify-between items-center h-screen overflow-hidden">
         <li className="h-screen bg-sky-700 flex justify-center items-center w-1/2 relative overflow-hidden">
-          <Link href="/pages/player-mode" className="text-neutral-50 text-7xl text-right block absolute bottom-0 right-0 w-96 p-8 leading-none font-black drop-shadow-xl">Manager mode</Link>
+          <Link href="/pages/manager-mode" className="text-neutral-50 text-7xl text-right block absolute bottom-0 right-0 w-96 p-8 leading-none font-black drop-shadow-xl">Manager mode</Link>
         </li>
         <li className="h-screen bg-red-800 flex justify-center items-center w-1/2 relative">
           <Link href="/pages/player-mode" className="text-neutral-50 text-7xl text-left block absolute top-0 left-0 w-96 p-8 leading-none font-black drop-shadow-xl">Player mode</Link>
---
+++ src/app/pages/manager-mode/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="manager-mode p-16">
+      <h1>Manager mode here !</h1>
+    </div>
+  )
+}
